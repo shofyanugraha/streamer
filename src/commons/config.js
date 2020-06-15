7be0139ddd9fb092c222d98799890a3e20284c27
Rizky Nugraha <rizkyshofyan@gmail.com>
2020-06-15 20:06:26 +0700

    typo env

--- src/commons/config.js
+++ src/commons/config.js
@@ -1,3 +1,3 @@
-export const URL = process.env.VUE_API_URL;
-export const API = `${process.env.VUE_API_URL}/api/v1/`;
+export const URL = process.env.VUE_APP_API_URL;
+export const API = `${process.env.VUE_APP_API_URL}/api/v1/`;
 export default URL;
